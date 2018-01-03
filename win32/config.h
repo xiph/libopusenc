545ff493697c440d570df0e8eb8bd276570537f9
Ricardo Constantino <wiiaboo@gmail.com>
2018-01-03 13:44:46 +0000

    Add Visual Studio 2015 projects
    Signed-off-by: Jean-Marc Valin <jmvalin@jmvalin.ca>

---
+++ win32/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Enable fixed-point calculation */
+/* #undef OP_FIXED_POINT */
+
+#define OPE_BUILD
+
+#define PACKAGE_NAME "libopusenc"
+
+#include "version.h"
+
+#endif /* CONFIG_H */
